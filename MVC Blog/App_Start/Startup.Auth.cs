ce9e6fb28ddf1bfbf0be4a86328a0d68e9b0518a
PhonchicK <34416318+PhonchicK@users.noreply.github.com>
2022-02-09 23:38:30 +0300

    Add project files.

---
+++ MVC Blog/App_Start/Startup.Auth.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(MVC_Blog.App_Start.Startup))]
+
+namespace MVC_Blog.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions() 
+            {
+                AuthenticationType = "ApplicationAuth",
+                LoginPath = new PathString("/Account/Login"),
+                LogoutPath = new PathString("/Account/Logout"),
+                ExpireTimeSpan = TimeSpan.FromHours(2.0)
+            });
+        }
+    }
+}
---
+++ MVC Blog/Identity/ApplicationIdentityContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Blog.Identity
+{
+    public class ApplicationIdentityContext : IdentityDbContext<ApplicationUser, ApplicationRole, 
+        string, IdentityUserLogin, IdentityUserRole, IdentityUserClaim>
+    {
+
+    }
+}
